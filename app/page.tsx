5f180780ecd6b66fdb05ef1c3f55a47bd0b2539f
udaysharma1501 <udaysharma1501@gmail.com>
2024-07-22 10:10:35 +0530

    navbar config

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <>
       <h1 className="text-2xl">dashboard</h1>
-      <Button variant="destructive" size="lg" className="text-black-400">
-        Click Me
-      </Button>
     </>
   );
 }
