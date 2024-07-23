2411b4f843ce484bd5ec97ac2dd96c56e5a7c231
udaysharma1501 <udaysharma1501@gmail.com>
2024-07-23 07:17:49 +0530

    dynamic cards 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,30 @@
 import DashboardCard from "@/components/dashboard/DashboardCard";
-import { Newspaper } from "lucide-react";
-
+import { Newspaper, Folder, User, MessageCircle } from "lucide-react";
 
 export default function Home() {
   return (
     <>
       <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
-
         <DashboardCard
           title="Posts"
           count={100}
           icon={<Newspaper className="text-slate-500" size={72} />}
         />
-        
+        <DashboardCard
+          title="Categories"
+          count={12}
+          icon={<Folder className="text-slate-500" size={72} />}
+        />
+        <DashboardCard
+          title="Users"
+          count={750}
+          icon={<User className="text-slate-500" size={72} />}
+        />
+        <DashboardCard
+          title="Comments"
+          count={1200}
+          icon={<MessageCircle className="text-slate-500" size={72} />}
+        />
       </div>
     </>
   );
